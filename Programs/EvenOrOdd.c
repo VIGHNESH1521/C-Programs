52c2eef28d6b5616724352d33c0df86215671f0b
VIGHNESH SUDHAKAR <vighneshsudhakar@gmail.com>
2022-07-30 20:52:01 +0530

    Add files via upload

---
+++ Programs/EvenOrOdd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int number;
+    printf("Enter the number : ");
+    scanf("%d", &number);
+
+    if(number % 2 == 0)
+    {
+        printf("The number %d is even.", number);
+    }
+    else
+    {
+        printf("The number %d is odd.", number);
+    }
+    return 0;
+}
